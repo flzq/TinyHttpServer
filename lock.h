be3e598c68ef16d060ab06431505f714bff15516
flzq <freedomcoder@163.com>
2021-05-22 15:27:09 +0800

    update

--- lock.h
+++ lock.h
@@ -13,6 +13,11 @@ public:
             throw std::exception();
         }
     }
+    Sem (int num) {
+        if (sem_init(&m_sem, 0, num) != 0) {
+            throw std::exception();
+        }
+    }
     // 销毁信号量
     ~Sem() {
         sem_destroy(&m_sem);
@@ -48,6 +53,9 @@ public:
     bool unlock() {
         return pthread_mutex_unlock(&m_mutex) == 0;
     }
+    pthread_mutex_t *get() {
+        return &m_mutex;
+    }
 
 private:
     pthread_mutex_t m_mutex;
@@ -56,31 +64,34 @@ private:
 class Cond {
 public:
     Cond() {
-        if (pthread_mutex_init(&m_mutex, NULL) != 0) {
-            throw std::exception();
-        }
+        // if (pthread_mutex_init(&m_mutex, NULL) != 0) {
+        //     throw std::exception();
+        // }
         if (pthread_cond_init(&m_cond, NULL) != 0) {
-            pthread_mutex_destroy(&m_mutex);
+            // pthread_mutex_destroy(&m_mutex);
             throw std::exception();
         }
     }
     ~Cond() {
-        pthread_mutex_destroy(&m_mutex);
+        // pthread_mutex_destroy(&m_mutex);
         pthread_cond_destroy(&m_cond);
     }
-    bool wait() {
+    bool wait(pthread_mutex_t *pmutex) {
         int ret = 0;
-        pthread_mutex_lock(&m_mutex);
-        ret = pthread_cond_wait(&m_cond, &m_mutex);
-        pthread_mutex_unlock(&m_mutex);
+        // pthread_mutex_lock(&m_mutex);
+        ret = pthread_cond_wait(&m_cond, pmutex);
+        // pthread_mutex_unlock(&m_mutex);
         return ret == 0;
     }
     bool signal() {
         return pthread_cond_signal(&m_cond) == 0;
     }
+    bool broadcast() {
+        return pthread_cond_broadcast(&m_cond) == 0;
+    }
 
 private:
-    pthread_mutex_t m_mutex;
+    // pthread_mutex_t m_mutex;
     pthread_cond_t m_cond;
 };
 
